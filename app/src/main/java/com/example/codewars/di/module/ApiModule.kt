39074fb9fcdd4563d77b7322f508de125b74cbe3
Guilherme <guiljlima@gmail.com>
2020-07-15 17:34:34 -0300

    adding interceptor

--- app/src/main/java/com/example/codewars/di/module/ApiModule.kt
+++ app/src/main/java/com/example/codewars/di/module/ApiModule.kt
@@ -6,14 +6,14 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
-import okhttp3.Cache
-import okhttp3.OkHttpClient
+import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
 import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -22,11 +22,26 @@ class ApiModule {
     companion object {
         const val CACHE_HTTP = "http_cache"
         private const val CACHE_SIZE: Long = 50 * 1024 * 1024
+        const val NAMED_INTERCEPTOR_CACHE_CONTROL = "NAMED_INTERCEPTOR_CACHE_CONTROL"
     }
 
     @Provides
     @Singleton
     fun provideCache(fileCache: File) = Cache(fileCache, CACHE_SIZE)
+
+    @Provides
+    @Singleton
+    @Named(NAMED_INTERCEPTOR_CACHE_CONTROL)
+    fun provideInterceptorCacheControl() = Interceptor { chain ->
+        val response = chain.proceed(chain.request())
+        val cacheControl = response.cacheControl
+
+        return@Interceptor if (cacheControl.isPublic) response
+        else response
+            .newBuilder()
+            .headers(response.headers)
+            .build()
+    }
 
     @Provides
     @Singleton
@@ -41,15 +56,17 @@ class ApiModule {
 
     @Provides
     @Singleton
-    fun provideOkhttpClient(cache: Cache): OkHttpClient {
+    fun provideOkhttpClient(
+        @Named(NAMED_INTERCEPTOR_CACHE_CONTROL) cacheControlInterceptor: Interceptor,
+        cache: Cache): OkHttpClient =
+            OkHttpClient
+                .Builder()
+                .addInterceptor(cacheControlInterceptor)
+                .cache(cache)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build()
 
-        val httpClient = OkHttpClient
-            .Builder()
-        httpClient.cache(cache)
-        httpClient.connectTimeout(30, TimeUnit.SECONDS)
-        httpClient.readTimeout(30, TimeUnit.SECONDS)
-        return httpClient.build()
-    }
 
     @Provides
     @Singleton
