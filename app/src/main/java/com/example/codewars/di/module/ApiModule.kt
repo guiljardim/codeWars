6412459976fb8b7308a8ce0c76f1dc00798252bf
Guilherme <guiljlima@gmail.com>
2020-07-15 17:27:21 -0300

    implement cache mode

--- app/src/main/java/com/example/codewars/di/module/ApiModule.kt
+++ app/src/main/java/com/example/codewars/di/module/ApiModule.kt
@@ -1,5 +1,6 @@
 package com.example.codewars.di.module
 
+import android.app.Application
 import com.example.codewars.util.Constants.BASE_URL
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
@@ -11,11 +12,26 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 class ApiModule {
+
+    companion object {
+        const val CACHE_HTTP = "http_cache"
+        private const val CACHE_SIZE: Long = 50 * 1024 * 1024
+    }
+
+    @Provides
+    @Singleton
+    fun provideCache(fileCache: File) = Cache(fileCache, CACHE_SIZE)
+
+    @Provides
+    @Singleton
+    fun provideFileCache(application: Application) = File(application.cacheDir, CACHE_HTTP)
+
     @Provides
     @Singleton
      fun provideGson(): Gson {
@@ -25,9 +41,11 @@ class ApiModule {
 
     @Provides
     @Singleton
-    fun provideOkhttpClient(): OkHttpClient {
+    fun provideOkhttpClient(cache: Cache): OkHttpClient {
 
-        val httpClient = OkHttpClient.Builder()
+        val httpClient = OkHttpClient
+            .Builder()
+        httpClient.cache(cache)
         httpClient.connectTimeout(30, TimeUnit.SECONDS)
         httpClient.readTimeout(30, TimeUnit.SECONDS)
         return httpClient.build()
